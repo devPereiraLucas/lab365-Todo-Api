39398b911a499cbdc9d3d843a023f9a1fcb1d544
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2024-03-26 21:27:54 -0300

    create TodoItemModel

---
+++ Models/TodoItemsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace lab365_Todo_Api.Models
+{
+    public class TodoItemsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
