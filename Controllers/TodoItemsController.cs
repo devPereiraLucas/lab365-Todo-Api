cf23ef5a923627b8d84428700b48535f7fc18a79
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2024-03-26 21:29:13 -0300

    create methode createTodoItem

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,29 @@
+﻿using lab365_Todo_Api.DTOs;
+using lab365_Todo_Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab365_Todo_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> createTodoItem(TodoItemDto todoItemDto)
+        {
+            Random random = new();
+            int randomId = random.Next(1, 1000);
+
+            TodoItemsModel todoItemsModel = new()
+            {
+                Id = randomId,
+                Name = todoItemDto.Name,
+                Active = todoItemDto.Active,
+                CreatedAt = DateTime.Now
+            };
+
+            return Ok(todoItemsModel.Id);
+        }
+    }
+}
