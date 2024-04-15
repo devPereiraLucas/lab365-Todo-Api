c4617627cb7895a58516bff057b65088315095b8
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2024-04-14 22:43:13 -0300

    created TodoContextDB

---
+++ Models/TodoContextDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lab365_Todo_Api.Models;
+
+public class TodoContextDB : DbContext
+{
+    public TodoContextDB(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<TodoItemsModel> TodoItemModels { get; set; }
+}
