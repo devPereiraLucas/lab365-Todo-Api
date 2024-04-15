d51595f8adda339372708a68410061c6d716ceb4
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2024-04-14 22:44:26 -0300

    created TodoItem entity mapping

--- Models/TodoItemsModel.cs
+++ Models/TodoItemsModel.cs
@@ -1,9 +1,18 @@
-﻿namespace lab365_Todo_Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace lab365_Todo_Api.Models;
+
+[Table("TodoItem")]
 public class TodoItemsModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Column(TypeName = "VARCHAR"), StringLength(250), Required]
     public string Name { get; set; }
+    [Required]
     public bool Active { get; set; }
+    [Required]
     public DateTime CreatedAt { get; set; }
 }
