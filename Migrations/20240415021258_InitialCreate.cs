096b0f84315ba8268f4bf27a8dff0396f05d937b
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2024-04-16 23:42:24 -0300

    add entity package sqlserver and create migrations

---
+++ Migrations/20240415021258_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace lab365_Todo_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Active", "CreatedAt", "Name" },
+                values: new object[,]
+                {
+                    { 1, false, new DateTime(2024, 4, 15, 2, 12, 57, 698, DateTimeKind.Utc).AddTicks(8671), "Item 1" },
+                    { 2, true, new DateTime(2024, 4, 15, 2, 12, 57, 698, DateTimeKind.Utc).AddTicks(8681), "Item 2" },
+                    { 3, false, new DateTime(2024, 4, 15, 2, 12, 57, 698, DateTimeKind.Utc).AddTicks(8685), "Item 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+        }
+    }
+}
---
+++ Migrations/20240415024030_UpdateCreatedAtTodoItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab365_Todo_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCreatedAtTodoItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2024, 4, 14, 23, 40, 30, 171, DateTimeKind.Local).AddTicks(2716));
+
+            migrationBuilder.UpdateData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2024, 4, 14, 23, 40, 30, 171, DateTimeKind.Local).AddTicks(2732));
+
+            migrationBuilder.UpdateData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2024, 4, 14, 23, 40, 30, 171, DateTimeKind.Local).AddTicks(2735));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2024, 4, 15, 2, 12, 57, 698, DateTimeKind.Utc).AddTicks(8671));
+
+            migrationBuilder.UpdateData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2024, 4, 15, 2, 12, 57, 698, DateTimeKind.Utc).AddTicks(8681));
+
+            migrationBuilder.UpdateData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2024, 4, 15, 2, 12, 57, 698, DateTimeKind.Utc).AddTicks(8685));
+        }
+    }
+}
